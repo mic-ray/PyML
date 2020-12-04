0af566e1a3328d83e0eaf92b41e8c18972a9ec2a
Raykhlin <michael.a.raykhlin@capgemini.com>
2020-12-04 16:39:27 +0100

    Added example model
    Model:
    - Added a LinearRegression model, that is fitted on the training data
    - The model does some predictions on the test data, which are evaluated
    using a metric provided by scikit aswell
    
    Feature data:
    - Had to only choose numerical values for the feature so removed category
    and country columns
    - Therefore also had to transform the join date from a date string into
    a timestamp float

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import pandas as pd
+from datetime import datetime
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import explained_variance_score
+
 
 # Import and clean data
 channels = pd.read_csv("data/channels.csv").rename(columns={"followers": "subscribers"})
-channels.country = channels.country.fillna("Unknown")
+# Filter out rows, where a join date is not provided
+channels = channels.loc[channels.join_date.notnull()]
 
-# Select features and target
-features = ["category_name", "country", "join_date", "videos"]
+# Converts a date string to a timestamp
+def convert_date_to_timestamp(date):
+    return datetime.fromisoformat(str(date)).timestamp()
+
+
+# Convert date strings to a float value
+channels.join_date = channels.join_date.apply(convert_date_to_timestamp)
+
+# Select (only numeric) features and target
+features = ["join_date", "videos"]
 X = channels[features]
 y = channels.subscribers
 
@@ -14,4 +27,11 @@ y = channels.subscribers
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.33, random_state=42
 )
-print(X_train, X_test, y_train, y_test)
+
+# Create a model and fit model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Predicate values and score model
+y_pred = model.predict(X_test)
+print(explained_variance_score(y_test, y_pred))
