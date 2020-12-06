6c0e5e7d903d49d7faf084f22c51de84c9c509d0
Raykhlin <michael.a.raykhlin@capgemini.com>
2020-12-06 16:55:38 +0100

    Added model to wine wine classification
    Wine classification:
    - Fixed access to the wine dataset as a DataFrame
    - Added a Gaussian Naive Bayes model for classification
    - The model is fitted by using the train data from the split and finally
    evaluated by using the score metric, which shows very good performance

--- wine_classification.py
+++ wine_classification.py
@@ -1,5 +1,26 @@
 import pandas as pd
 from sklearn.datasets import load_wine
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
-wines = load_wine(as_frame=True)
-print(wines)
+# Load wine data as a DataFrame
+wines = load_wine(as_frame=True)["frame"]
+
+# Select features and target
+X = wines.drop("target", axis=1)
+y = wines.target
+
+# Split train/test data (40% test data)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.4, random_state=42
+)
+
+# Create and fit model
+model = GaussianNB()
+model.fit(X_train, y_train)
+
+# Predict and score accuracy
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
